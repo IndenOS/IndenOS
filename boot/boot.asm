5aa066b6e210360c27f43e4510dbc79a34cdf0b1
Rzd <140939924+MDrzd@users.noreply.github.com>
2025-02-05 00:17:20 +0700

    Protected Mode 32bit

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,24 +1,53 @@
-[BITS 16]      ; Mode real (16-bit)
-[ORG 0x7C00]   ; BIOS akan memuat bootloader di alamat ini
+[BITS 16]    
+[ORG 0x7C00] 
 
 start:
-    mov si, msg
+    cli              
+    lgdt [gdt_descriptor] 
+    mov eax, cr0     
+    or  eax, 1       
+    mov cr0, eax     
+    jmp CODE_SEG:init_protected_mode  
+
+[BITS 32]  
+init_protected_mode:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
+
+    mov esi, message
     call print_string
 
     jmp $
 
 print_string:
-    mov ah, 0x0E  ; BIOS teletype mode
+    mov ah, 0x0E
 .loop:
-    lodsb         ; Ambil karakter dari SI
+    lodsb
     cmp al, 0
-    je done       ; Jika null, selesai
-    int 0x10      ; Cetak karakter ke layar
+    je done
+    int 0x10
     jmp .loop
 done:
     ret
 
-msg db "Bootloader berhasil dimuat!", 0
+message db "Berhasil masuk Protected Mode!", 0
 
-times 510-($-$$) db 0  ; Padding agar ukuran tepat 512 byte
-dw 0xAA55              ; Boot signature
+gdt_start:
+    dq 0x0000000000000000  
+    dq 0x00CF9A000000FFFF  
+    dq 0x00CF92000000FFFF  
+
+gdt_descriptor:
+    dw gdt_descriptor - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ 0x08
+DATA_SEG equ 0x10
+
+times 510-($-$$) db 0
+dw 0xAA55
