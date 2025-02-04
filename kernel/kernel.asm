c2f3b1cf252365469a44f9cf689449b3d56e5497
Rzd <140939924+MDrzd@users.noreply.github.com>
2025-02-05 00:21:50 +0700

    Update kernel.asm

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,1 +1,8 @@
+[BITS 32]
+[GLOBAL _start]
+[EXTERN main]   ; Deklarasikan bahwa main() ada di file lain (kernel.c)
 
+SECTION .text
+_start:
+    call main   ; Panggil fungsi main() dari kernel.c
+    hlt         ; Hentikan CPU
